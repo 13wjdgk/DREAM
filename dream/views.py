eddf9c0beca7e340ea1936b85ceefe6c5bd7c85f
gyuyoung2 <tkrhk2836@naver.com>
2021-06-26 21:39:04 +0900

    Info

--- dream/views.py
+++ dream/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Info(request):
+    return render(request,'Info.html')
+
 def main(request):
     return render(request,'main.html')
